3f9f29694837e0e887ea45abf5af9e60d90bc61b
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2025-12-05 15:17:10 +0000

    Add dependency manifest and test configuration

--- src/index.js
+++ src/index.js
@@ -53,3 +53,15 @@ class PoolWatcher {
       }
       return true;
     } catch (err) {
+      logger.error('Pipeline failed', { error: err.message });
+      return false;
+    }
+  }
+}
+
+if (require.main === module) {
+  const app = new PoolWatcher();
+  app.run().then((ok) => process.exit(ok ? 0 : 1));
+}
+
+module.exports = { PoolWatcher, PoolWatcherCore };
